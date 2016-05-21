79835657ade721d18909106f126f48a06ad1520d
bidyeot13 <hassanbidyeot@gmail.com>
2016-05-21 19:29:53 +0600

    Add files via upload

---
+++ api/config.php
@@ -0,0 +1,10 @@
+﻿<?php
+	$app_id = "1722350238025805";
+	$api_secret = "3785743700eeb3f2752d39c5825a9ceb";
+	
+	$baseURL = "https://apps.facebook.com/bidpropic/";
+	$http_server = "http://autorent-bd.com/fbpic/";	
+	$secure_url = "http://autorent-bd.com/fbpic/";
+	$fanpage_url = "https://www.facebook.com/bidyeot";
+	$app_name = "Facebook Application";
+?>
