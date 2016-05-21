d8f4ed3afce0449374d1714dc21b936b411d0b43
bidyeot13 <hassanbidyeot@gmail.com>
2016-05-21 21:51:03 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,80 +1,60 @@
-<?php
+<?php 
 session_start();
-require_once __DIR__ . '/src/autoload.php';
-$fb = new Facebook\Facebook([
-  'app_id' => 'APP_ID',
-  'app_secret' => 'APP_SECRET',
-  'default_graph_version' => 'v2.4',
-]);
-$helper = $fb->getCanvasHelper();
-$permissions = ['email', 'publish_actions']; // optional
-try {
-	if (isset($_SESSION['facebook_access_token'])) {
-	$accessToken = $_SESSION['facebook_access_token'];
-	} else {
-  		$accessToken = $helper->getAccessToken();
-	}
-} catch(Facebook\Exceptions\FacebookResponseException $e) {
- 	// When Graph returns an error
- 	echo 'Graph returned an error: ' . $e->getMessage();
-  	exit;
-} catch(Facebook\Exceptions\FacebookSDKException $e) {
- 	// When validation fails or other local issues
-	echo 'Facebook SDK returned an error: ' . $e->getMessage();
-  	exit;
- }
-if (isset($accessToken)) {
-	if (isset($_SESSION['facebook_access_token'])) {
-		$fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
-	} else {
-		$_SESSION['facebook_access_token'] = (string) $accessToken;
-	  	// OAuth 2.0 client handler
-		$oAuth2Client = $fb->getOAuth2Client();
-		// Exchanges a short-lived access token for a long-lived one
-		$longLivedAccessToken = $oAuth2Client->getLongLivedAccessToken($_SESSION['facebook_access_token']);
-		$_SESSION['facebook_access_token'] = (string) $longLivedAccessToken;
-		$fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
-	}
-	
-	// validating the access token
-	try {
-		$request = $fb->get('/me');
-	} catch(Facebook\Exceptions\FacebookResponseException $e) {
-		// When Graph returns an error
-		if ($e->getCode() == 190) {
-			unset($_SESSION['facebook_access_token']);
-			$helper = $fb->getRedirectLoginHelper();
-			$loginUrl = $helper->getLoginUrl('https://apps.facebook.com/APP_NAMESPACE/', $permissions);
-			echo "<script>window.top.location.href='".$loginUrl."'</script>";
-			exit;
-		}
-	} catch(Facebook\Exceptions\FacebookSDKException $e) {
-		// When validation fails or other local issues
-		echo 'Facebook SDK returned an error: ' . $e->getMessage();
-		exit;
-	}
+require_once "src/facebook.php"; 
 
-	try {
-		// message must come from the user-end
-		$data = ['source' => $fb->fileToUpload(__DIR__.'/photo.jpg'), 'message' => 'my photo'];
-		$request = $fb->post('/me/photos', $data);
-		$response = $request->getGraphNode()->asArray();
-	} catch(Facebook\Exceptions\FacebookResponseException $e) {
-		// When Graph returns an error
-		echo 'Graph returned an error: ' . $e->getMessage();
-		exit;
-	} catch(Facebook\Exceptions\FacebookSDKException $e) {
-		// When validation fails or other local issues
-		echo 'Facebook SDK returned an error: ' . $e->getMessage();
-		exit;
-	}
+$config = array(
+    "appId" => FACEBOOK_APP_ID,
+    "secret" => FACEBOOK_APP_SECRET);
 
-	echo $response['id'];
+$fb = new Facebook($config);
 
-  	// Now you can redirect to another page and use the
-  	// access token from $_SESSION['facebook_access_token']
-} else {
-	$helper = $fb->getRedirectLoginHelper();
-	$loginUrl = $helper->getLoginUrl('https://apps.facebook.com/APP_NAMESPACE/', $permissions);
-	echo "<script>window.top.location.href='".$loginUrl."'</script>";
+$user = $fb->getUser();
+?>
+<html> 
+ <head> 
+  <title>Hello Facebook</title> 
+ </head> 
+ <body>
+<?php
+if (!$user) { 
+    $params = array(
+        "scope" => "read_stream,publish_stream,user_photos",
+        "redirect_uri" => REDIRECT_URI);
+    echo '<a href="' . $fb->getLoginUrl($params) . '">Login</a>'; 
 }
+else { 
+?>
+  <form action="<?php echo $_SERVER["PHP_SELF"];?>" method="post" enctype="multipart/form-data">
+   <textarea name="message" id="message" rows="2" cols="40"></textarea><br>
+   <input type="file" name="image" id="image"><br>
+   <input type="submit" value="Update"> 
+  </form> 
+<?php
+    // process form submission
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST["message"])) {
+        if (is_uploaded_file($_FILES["image"]["tmp_name"])) { 
+            $finfo = finfo_open(FILEINFO_MIME_TYPE);
+            $mime = finfo_file($finfo, $_FILES["image"]["tmp_name"]);
+            $allowed = array("image/gif", "image/jpg", "image/jpeg", "image/png");
+            // upload image
+            if (in_array($mime, $allowed)) { 
+                $data = array(
+                    "name" => $_POST["message"],
+                    "image" => "@" . realpath($_FILES["image"]["tmp_name"]));
+                $fb->setFileUploadSupport(true); 
+                $status = $fb->api("/me/photos", "POST", $data);    
+            }
+        }
+        else {
+            // update status message
+            $data = array("message" => $_POST["message"]);
+            $status = $fb->api("/me/feed", "POST", $data); 
+        }
+    } 
+    if (isset($status)) {
+        echo "<pre>" . print_r($status, true) . "</pre>";
+    } 
+}
+?>
+ </body> 
+</html>
