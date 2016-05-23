5f1b4b7b9908564ce52055b3b70eba5fdabede21
bidyeot13 <hassanbidyeot@gmail.com>
2016-05-23 11:39:03 +0600

    Update index.php

--- index.php
+++ index.php
@@ -4,13 +4,20 @@ require 'src/Facebook.php';
 $fb = new Facebook\Facebook([
   'app_id' => '1722350238025805',
   'app_secret' => '3785743700eeb3f2752d39c5825a9ceb',
-  'default_graph_version' => 'v2.2',
+  'default_access_token' => '867666c92891f723fabb2fc132875bea',
+  'enable_beta_mode' => true,
+  'default_graph_version' => 'v2.3',
+  'http_client_handler' => 'guzzle',
+  'persistent_data_handler' => 'memory',
+  'url_detection_handler' => new MyUrlDetectionHandler(),
+  'pseudo_random_string_generator' => new MyPseudoRandomStringGenerator(),
+  
   ]);
   
 
 $data = [
   'message' => 'My awesome photo upload example.',
-  'source' => $fb->fileToUpload('koala.jpg'),
+  'source' => $fb->fileToUpload('/koala.jpg'),
 ];
 
 try {
