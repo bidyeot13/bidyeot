d9c6bed473c8b7e09922df70680bfafe4e58ad2e
bidyeot13 <hassanbidyeot@gmail.com>
2016-05-23 12:32:35 +0600

    Add files via upload

---
+++ src/config.php
@@ -0,0 +1,8 @@
+<?php
+$config['callback_url']         =   '$config['callback_url'] = "http://https://bidyeot.herokuapp.com/index.php/?fbTrue=true"'; //   /?fbTrue=true is required.
+
+//Facebook configuration
+$config['App_ID']      =   '1722350238025805';
+$config['App_Secret']  =   '3785743700eeb3f2752d39c5825a9ceb'; 
+
+?>
