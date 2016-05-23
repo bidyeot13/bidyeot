c4ec92b0ee9d4d81c82515915039aa352fc5a19a
bidyeot13 <hassanbidyeot@gmail.com>
2016-05-23 12:10:52 +0600

    Create config.php

---
+++ php-config-callback_url-CALL-BACK-URL/fbTrue-true/fbTrue-true-allow-you-to-code-process-section./config.php
@@ -0,0 +1,9 @@
+<?php
+ 
+$config['callback_url']         =   'https://bidyeot.herokuapp.com/index.php/?fbTrue=true'; //    /?fbTrue=true allow you to code process section.
+ 
+//Facebook configuration
+$config['App_ID']      =   '1722350238025805';
+$config['App_Secret']  =   '3785743700eeb3f2752d39c5825a9ceb'; 
+ 
+?>
