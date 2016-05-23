1161b45d268c21c21645914c7d48bdad88fee8a4
bidyeot13 <hassanbidyeot@gmail.com>
2016-05-23 11:31:41 +0600

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,6 @@ $fb = new Facebook\Facebook([
   'app_id' => '1722350238025805',
   'app_secret' => '3785743700eeb3f2752d39c5825a9ceb',
   'default_graph_version' => 'v2.2',
-  		"cookie" => true,
-		'fileUpload' => true,
   ]);
   
 
